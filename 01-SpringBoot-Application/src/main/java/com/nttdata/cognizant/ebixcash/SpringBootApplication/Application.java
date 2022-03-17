2ce2d804bb8abd8ae8b88ddfb02ba646ea11349c
unknown <ramumallina9999@gmail.com>
2022-03-17 22:26:00 +0530

    Spring Boot Code is Committing

---
+++ 01-SpringBoot-Application/src/main/java/com/nttdata/cognizant/ebixcash/SpringBootApplication/Application.java
@@ -0,0 +1,13 @@
+package com.nttdata.cognizant.ebixcash.SpringBootApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ 01-SpringBoot-Application/src/main/java/com/nttdata/cognizant/ebixcash/SpringBootApplication/User.java
@@ -0,0 +1,12 @@
+package com.nttdata.cognizant.ebixcash.SpringBootApplication;
+
+import org.springframework.stereotype.Component;
+
+@Component
+
+public class User {
+
+	public User() {
+		System.out.println("User Default Constructor:::");
+	}
+}
